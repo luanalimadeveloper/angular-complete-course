5a8cc35dee6f6cb8e39d62f02303c9b9eab8f69d
Luana Lima <luanalima.developer@gmail.com>
2024-03-10 18:03:59 -0300

    Types of Component Selector | Angular Components & Directives - selector like css class

--- src/app/top-header/top-header.component.ts
+++ src/app/top-header/top-header.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: '[top-header]',
+  selector: '.top-header',
   templateUrl: './top-header.component.html',
   styleUrls: ['./top-header.component.css']
 })
