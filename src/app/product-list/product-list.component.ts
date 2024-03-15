e57647a989e39b6accf08a7c94bae88a4aa8fd03
Luana Lima <luanalima.developer@gmail.com>
2024-03-15 00:00:01 -0300

    String Interpolation | Angular Components & Directives

---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+ name: string = 'iPhone 13';
+ price: number = 999;
+ color: string = 'Red';
+}
