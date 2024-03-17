247f771de8b1f5f7e3690c70de873061cd9c9fe2
Luana Lima <luanalima.developer@gmail.com>
2024-03-16 23:02:59 -0300

    Event Binding | Angular Components & Directives

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NumberValueAccessor } from '@angular/forms';
 
 @Component({
   selector: 'product-list',
@@ -6,7 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
-  name="John Doe"
+  // name="Luana"
+  addToCart: number = 0;
   product = {
     name: 'Iphone X',
     price: 789,
@@ -19,4 +21,25 @@ export class ProductListComponent {
   getDiscountedPrice() {
     return this.product.price - (this.product.price * this.product.discount / 100)
   }
+
+  onNameChange(event: any){
+    //console.log(event.target.value);
+    //this.name = event.target.value;
+  }
+
+  decrementCartValue(){
+    if(this.addToCart > 0){
+      this.addToCart--;
+    }
+  }
+
+  incrementCartValue(){
+    if(this.addToCart < this.product.inStock) {
+      this.addToCart++
+    }
+  }
 }
+
+
+
+
