df6e153f224d9a057bbbc4f4d786b490cf4b8fe6
Luana Lima <luanalima.developer@gmail.com>
2024-03-20 23:07:30 -0300

    ngFor Directive | Angular Components & Directives

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -7,37 +7,7 @@ import { NumberValueAccessor } from '@angular/forms';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
-  // name="Luana"
-  addToCart: number = 0;
-  product = {
-    name: 'Iphone X',
-    price: 789,
-    color: 'Black',
-    discount: 8.5,
-    inStock: 10,
-    pImage: '/assets/images/iphone.png',
-  }
-
-  getDiscountedPrice() {
-    return this.product.price - (this.product.price * this.product.discount / 100)
-  }
-
-  onNameChange(event: any){
-    //console.log(event.target.value);
-    //this.name = event.target.value;
-  }
-
-  decrementCartValue(){
-    if(this.addToCart > 0){
-      this.addToCart--;
-    }
-  }
-
-  incrementCartValue(){
-    if(this.addToCart < this.product.inStock) {
-      this.addToCart++
-    }
-  }
+  listOfString: string[] = ['Mark', 'Severino', 'Luana', 'Joe', 'Monica'];
 }
 
 
