f852283bfa7263e91235429394495abcde6fae5d
Luana Lima <luanalima.developer@gmail.com>
2024-03-27 19:44:56 -0300

    ngIf Directive | Angular Components & Directives | A Complete Angular Course

---
+++ src/app/container/container.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { NumberValueAccessor } from '@angular/forms';
+
+@Component({
+  selector: 'app-container',
+  templateUrl: './container.component.html',
+  styleUrls: ['./container.component.css']
+})
+export class ContainerComponent {
+  listOfString: string[] = ['Mark', 'Severino', 'Luana', 'Joe', 'Monica'];
+}
+
+
+
+
